8ceb6174482a4a37e807a456430397e61987367f
Kenichi Yamada <75155258+r-40021@users.noreply.github.com>
2021-05-08 17:03:10 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+var CACHE_NAME = "20210508",
+    urlsToCache = ["./", 
+                   "./assets/css/style.css", 
+                   "./favicon/favicon-32x32.png",
+                   "./uploads/MIppxngn_400x400.jpg", 
+                   "./favicon/site.webmanifest",
+                  "./uploads/2021/05/08/bingo_ja.jpg",
+                  "./uploads/timer.png",
+                  "./uploads/fake.jpg",
+                  "./uploads/key.PNG"];
+
+
+self.addEventListener("install", (function(event) {
+    event.waitUntil(caches.open(CACHE_NAME).then((function(cache) {
+        return console.log("Opened cache"), cache.addAll(urlsToCache)
+    })))
+})), self.addEventListener("activate", (function(event) {
+    var cacheAllowlist = [CACHE_NAME];
+    event.waitUntil(caches.keys().then((function(cacheNames) {
+        return Promise.all(cacheNames.map((function(cacheName) {
+            if (-1 === cacheAllowlist.indexOf(cacheName)) return caches.delete(cacheName)
+        })))
+    })))
+})), self.addEventListener("fetch", (function(event) {
+    event.respondWith(caches.match(event.request).then((function(response) {
+        if (response) return response;
+        var fetchRequest = event.request.clone();
+        return fetch(fetchRequest).then((function(response) {
+            if (!response || 200 !== response.status || "basic" !== response.type) return response;
+            var responseToCache = response.clone();
+            return caches.open(CACHE_NAME).then((function(cache) {
+                cache.put(event.request, responseToCache)
+            })), response
+        }))
+    })))
+}));
