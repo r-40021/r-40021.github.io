0dc63cc13f528044ccc2ed8ea0aa1ccc2ed6a35e
Kenichi Yamada <75155258+r-40021@users.noreply.github.com>
2021-05-14 20:48:37 +0900

    アプリからの遷移時にヘッダーを隠す

---
+++ assets/js/hideMenu.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+const query = new URL(window.location.href).searchParams.get("fromApp");
+if (query === "true") {
+    document.getElementsByTagName("header")[0].style.display = "none";
+    const alinks = document.getElementsByTagName("a");
+    for (let i = 0; i < alinks.length; i++) {
+        var element = alinks[i];
+        if (element.href.indexOf(location.hostname) !== -1 && !element.target) {
+            var href = element.href;
+            if (href.indexOf("#") !== -1) {
+                var hrefHash = href.split("#");
+                element.setAttribute("href", hrefHash[0] + "?fromApp=true#" + hrefHash[1])
+            } else {
+                element.setAttribute("href",href + "?fromApp=true")
+            }
+        }
+        
+    }
+}
+},false);
