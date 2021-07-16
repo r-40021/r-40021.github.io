115f4b627fbff33f1f078ce127f6949ec3285b7b
Kenichi Yamada <75155258+r-40021@users.noreply.github.com>
2021-07-16 22:09:06 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+if (location.pathname.indexOf("countdown-timer") === -1 && location.pathname.indexOf("bingo") === -1 && location.pathname.indexOf("stopwatch") === -1) {
 navigator.serviceWorker.getRegistrations().then(function(registrations) {
     // 登録されているworkerを全て削除する
     for(let registration of registrations) {
@@ -13,3 +14,4 @@ caches.keys().then(function(keys) {
         }
     });
 });
+}
