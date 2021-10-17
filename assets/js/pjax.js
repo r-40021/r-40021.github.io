9ca953a5d97087e22a0e1893cf8311845dbf40ae
Frog <75155258+r-40021@users.noreply.github.com>
2021-10-17 20:36:13 +0900

    update

--- assets/js/pjax.js
+++ assets/js/pjax.js
@@ -31,7 +31,10 @@ barba.init({
         if(/Timeout error/.test(response)){
             barba.force(url); // タイムアウト時にリダイレクト
         }
+        trigger.preventDefault();
+        action.preventDefault();
         e.preventDefault();
+        event.preventDefault();
         return false;
       },
     transitions: [{
