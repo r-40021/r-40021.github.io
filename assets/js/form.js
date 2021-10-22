061a7858ed3fa9d9b11b92dc04f0252ed6c6d3c9
Frog <75155258+r-40021@users.noreply.github.com>
2021-10-23 00:23:56 +0900

    [Update] js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,7 +1,8 @@
 document.getElementById("contact").addEventListener("submit", (e) => {
-    document.documentElement.style.scrollBehavior = "auto";
+    // document.documentElement.style.scrollBehavior = "auto";
     const form = document.getElementById("contact");
-    const formData = new FormData(form);
+    const formData = new FormData(form); //フォームの入力内容
+    // 各種ログを非表示に
     document.getElementById("success-message").style.display = "";
     document.getElementById("error-message").style.display = "";
     document.getElementById("empty-message").style.display = "";
@@ -12,17 +13,17 @@ document.getElementById("contact").addEventListener("submit", (e) => {
       document.getElementById("form-message").value
     ) {
       const submitBtn = document.getElementById("form-submit");
-      submitBtn.setAttribute("disabled", true);
-      submitBtn.setAttribute("value", "送信中...");
+      submitBtn.setAttribute("disabled", true); //送信ボタンを無効化
+      submitBtn.setAttribute("value", "送信中..."); //送信ボタンのテキストを「送信中...」に
       var xhr = new XMLHttpRequest();
       xhr.addEventListener("readystatechange", () => {
         if (xhr.readyState === 4 && xhr.status === 0) {
-          submitBtn.remove();
-          document.getElementById("success-message").style.display = "block";
+          submitBtn.remove();//送信ボタンを非表示に
+          document.getElementById("success-message").style.display = "block"; // 成功メッセージを表示
         } else if (xhr.readyState === 4) {
-          submitBtn.removeAttribute("disabled");
+          submitBtn.removeAttribute("disabled"); //送信ボタンを有効化
           submitBtn.setAttribute("value", "送信");
-          document.getElementById("error-message").style.display = "block";
+          document.getElementById("error-message").style.display = "block"; // エラーメッセージを表示
         }
       });
   
@@ -31,9 +32,9 @@ document.getElementById("contact").addEventListener("submit", (e) => {
       xhr.timeout = 5000;
       xhr.send(formData);
     } else {
-      document.getElementById("empty-message").style.display = "block";
+      document.getElementById("empty-message").style.display = "block"; //必要事項漏れを通知
     }
     e.preventDefault();
-    document.documentElement.style.scrollBehavior = "";
+    // document.documentElement.style.scrollBehavior = "";
   });
   
