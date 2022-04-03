7872c13fc7c8cb9d430b0532fd7e207768675635
Frog <75155258+r-40021@users.noreply.github.com>
2022-04-03 14:02:47 +0900

    [Update] Move script to pjax.js

--- assets/js/pjax.js
+++ assets/js/pjax.js
@@ -90,7 +90,13 @@ barba.hooks.after(() => {
         link.addEventListener('click', e => {
             eventDelete(e);
         }, false)
-    })
+    });
+    const domain = document.domain;
+    const outsideLink = document.querySelectorAll('.block-hero-2 .columns a[href^=http]'); // Hero 内の外部リンクを取得
+    outsideLink.forEach(elem => {
+        elem.setAttribute('target', '_blank');
+        elem.setAttribute('rel', 'noopener noreferrer');
+    });
 })
 
 // 同じURLのときは遷移しない
